97d12b447bc4dc923e02bd5fdb8437cfd80ff066
Akhil Goel <akhil.goel@intel.com>
2025-11-04 07:29:59 -0800

    PR #32934: [XLA:CPU][oneDNN] Fix failing oneDNN tests and F16 regressions
    Imported from GitHub PR https://github.com/openxla/xla/pull/32934
    
    This PR addresses the side effects of a previous XLA commit (https://github.com/openxla/xla/commit/bd3f15e30fd588a53c8d7a594bafaa34d4f4adb5) that prevented any fusions for oneDNN F16 custom calls. In particular, this PR:
    1. Expands the type promotion condition to include checks for oneDNN CCs too.
    2. Use oneDNN float support for both oneDNN CCs and graph based execution.
    Copybara import of the project:
    
    --
    00c688e5c73b802591f6266e46e264acc6ab3052 by Akhil Goel <akhil.goel@intel.com>:
    
    Initial commit
    
    Merging this change closes #32934
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/32934 from Intel-tensorflow:akhil/modify_change_op_type_condition 00c688e5c73b802591f6266e46e264acc6ab3052
    PiperOrigin-RevId: 827964896

--- xla/service/cpu/cpu_compiler.cc
+++ xla/service/cpu/cpu_compiler.cc
@@ -726,6 +726,9 @@ absl::Status CpuCompiler::RunHloPassesThroughLayoutAssn(
           .debug_options()
           .xla_cpu_experimental_onednn_custom_call() &&
       IsOneDnnCompatible(is_aot_compile);
+  bool use_onednn_graph =
+      module->config().debug_options().xla_cpu_use_onednn() &&
+      IsOneDnnCompatible(is_aot_compile);
 #ifdef XLA_ONEDNN
   if (use_onednn_custom_call) {
     // Placing OneDnnOpsRewriter here to match the flax patterns
@@ -747,7 +750,7 @@ absl::Status CpuCompiler::RunHloPassesThroughLayoutAssn(
   CpuFloatSupport bf16_support(BF16, call_library_for_dot);
 #ifdef XLA_ONEDNN
   OneDnnFloatSupport onednn_bf16_support(BF16);
-  if (use_onednn_custom_call) {
+  if (use_onednn_custom_call || use_onednn_graph) {
     pipeline.AddPass<FloatNormalization>(&onednn_bf16_support);
   } else {
     pipeline.AddPass<FloatNormalization>(&bf16_support);
