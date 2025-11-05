9b0985b00577312f258468e57f4c4ca22b47bbe5
Your Name <you@example.com>
2025-11-04 19:27:09 -0500

    xla cpu cpu features

--- xla/hlo/tools/hlo_aot.cc
+++ xla/hlo/tools/hlo_aot.cc
@@ -2,14 +2,17 @@
 #include <fstream>
 #include <sstream>
 #include "absl/strings/string_view.h"
+#include "absl/strings/str_split.h"
 #include "llvm/Support/CodeGen.h"  // For Reloc
 #include "xla/hlo/parser/hlo_parser.h"
 #include "xla/service/compile_only_service.h"
 #include "xla/service/cpu/cpu_aot_compilation_result.h"
 #include "xla/service/compiler.h"
 #include "xla/stream_executor/platform_manager.h"  // Added for PlatformManager
+#include "llvm/TargetParser/Host.h"  // For
+#include "llvm/ADT/StringMap.h"
+#include <iostream>
 
-using namespace xla;
 int main(int argc, char** argv) {
 
   absl::string_view hlo = R"(
@@ -21,7 +24,16 @@ int main(int argc, char** argv) {
       ROOT c = f32[2,2] dot(a, b), lhs_contracting_dims={1}, rhs_contracting_dims={0}
     }
   )";
-  std::unique_ptr<AotCompilationOptions> aot_options;
+
+
+  llvm::StringMap<bool, llvm::MallocAllocator> host_machine_features = llvm::sys::getHostCPUFeatures();
+
+  std::unique_ptr<xla::AotCompilationOptions> aot_options;
+  aot_options = std::make_unique<xla::cpu::CpuAotCompilationOptions>(
+      /*triple=*/"x86_64-unknown-linux-gnu", /*cpu_name=*/"skylake-avx512",
+      /*features=*/"avx512f,avx512vl",
+      /*entry_point_name=*/"dot_f32",
+      /*relocation_model=*/xla::cpu::CpuAotCompilationOptions::RelocationModel::Static);
 
 
 
